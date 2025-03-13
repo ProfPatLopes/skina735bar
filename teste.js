a8246baf6d816f5eca0e39888c824cdf946f9a41
Patrícia Lopes <102095019+ProfPatLopes@users.noreply.github.com>
2025-03-13 07:20:56 -0300

    .

--- teste.js
+++ teste.js
@@ -55,7 +55,7 @@
 
     function atualizarPainel() {
         const painel = document.getElementById('itensPedido');
-        //painel.innerHTML = '';
+        painel.innerHTML = '';
         produtosSelecionados.forEach(item => {
             painel.innerHTML += `<li>${item.nome} - Quantidade: ${item.qtd} - Valor: R$${item.valor.toFixed(2)}</li>`;
         });
@@ -103,7 +103,7 @@
     }
 
     function atualizarTotal() {
-            let total;
+            let total;//=0
             const produtos = document.querySelectorAll('.produto');
 
             produtos.forEach(produto => {
