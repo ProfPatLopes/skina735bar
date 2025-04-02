fe8681a2216d7fc747171443329ea3909353172b
Patrícia Lopes <102095019+ProfPatLopes@users.noreply.github.com>
2025-04-02 10:00:56 -0300

    xx

--- teste.js
+++ teste.js
@@ -278,8 +278,7 @@ function imprimirPedido2() {
     detalhesPedido += `
         <table style="width: auto; font-size: 22px;">
             <tr>
-                <td style="text-align: right; width: 30%;"><img src="Logo.png" style="width: 100%; height: auto;"></td>
-                <td colspan="3" style="font-size: 22px; text-align: left; padding: 15px""><strong>PEDIDO</strong></td>
+                <td colspan="4" style="font-size: 22px; text-align: left; padding: 15px""><strong>PEDIDO</strong></td>
             </tr>
             <tr >
                 <td colspan="4" style="width: auto;font-size: 18px; text-align: center; width: 100%;">${data}</td>
@@ -295,12 +294,6 @@ function imprimirPedido2() {
             <tr>
                 <td colspan="4" style="width: auto;">-------------------------------------------------</td>
 
-            </tr>
-            
-            <tr font-size: 22px; style="width: auto;">
-                <td colspan="2" style="text-align: left;">Produto</td>
-                <td style="text-align: right;">Unit</td>
-                <td style="text-align: right;">Total</td>
             </tr>
             <tr>
                 <td colspan="4" style="width: auto;">-------------------------------------------------</td>
