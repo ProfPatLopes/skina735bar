037ed9997a1bc958849e4b02c5f049bbb39f8990
Patrícia Lopes <102095019+ProfPatLopes@users.noreply.github.com>
2025-04-02 13:29:58 -0300

    botao

--- teste.js
+++ teste.js
@@ -130,7 +130,7 @@ function exibirProdutos() {
         <td><input type="checkbox" id="produto${index}" data-tipo="${produto.tipo}" data-produto="${produto.nome}" data-preco="${produto.preco}" onchange="atualizarTotal()"></td>
         <td style='width:80%'><label for="produto${index}">${produto.nome} (R$${produto.preco.toFixed(2)})</label></td>
         <td>Qtd: <input type="number" id="quantidade${index}" value="0" min="0" onchange="atualizarTota</td>
-        </table>
+        <td><button class="botao" onclick="adicionarItens()"><img src="adiciona.png" alt="Adicionar" width="10" height="10"></button></td></table>
     </div>`;
     listaProdutos.innerHTML += produtoHTML;
                     
