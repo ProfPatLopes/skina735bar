38913b2fd34c1924666e7cb8e0c8c166753b2fa6
Patrícia Lopes <102095019+ProfPatLopes@users.noreply.github.com>
2025-04-03 13:35:45 -0300

    zz

--- teste.js
+++ teste.js
@@ -208,10 +208,11 @@ function imprimirPedido2() {
         <table style="width: auto; font-size: 22px;">
             <tr>
                 <td style="text-align: right; width: 30%;"><img src="Logo.png" style="width: 100%; height: auto;"></td>
-                <td colspan="3" style="font-size: 12px; text-align: left; padding: 15px""><strong>Skina 735 Bar - Rua Goioerê esquina com a Norte do Paraná, 735 - Bairro Jd. Curitiba - Goioerê-Pr CEP 87,360-000 - CNPJ: 14.451.465/0001-73</strong></td>
+                <td colspan="3" style="font-size: 6px; text-align: justufy; padding: 15px""><strong>Skina 735 Bar - Rua Goioerê esquina com a Norte do Paraná, 735 - Bairro Jd. Curitiba - Goioerê-Pr CEP 87,360-000 - CNPJ: 14.451.465/0001-73</strong></td>
             </tr>
-            <p style="background-color: black; color: white;text-align: center; font-size: 22px;"><strong>PEDIDO</strong></p>
-
+            <tr>
+            <td colspan='4' style="background-color: black; color: white;text-align: center; font-size: 16px;"><strong>PEDIDO</strong></td>
+            </tr>
             <tr >
                 <td colspan="4" style="width: auto;font-size: 18px; text-align: center; width: 100%;">${data}</td>
             </tr>
