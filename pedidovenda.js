d5c536accd7cf65c61a482b0c4c750bb5468cb64
Patrícia Lopes <102095019+ProfPatLopes@users.noreply.github.com>
2025-04-07 15:21:35 -0300

    18px

--- pedidovenda.js
+++ pedidovenda.js
@@ -124,18 +124,17 @@ function exibirProdutos() {
                 const produtos = produtosPorCategoria[categoria];
                 produtos.forEach((produto, index) => {
                     const produtoHTML = `
-    <div class="produto" style="width:100%">
-        <table style="width:100%">
-            <tr style="width:100%">
-                <td style="width:auto"><input type="checkbox" id="produto${index}" data-tipo="${produto.tipo}" data-produto="${produto.nome}" data-preco="${produto.preco}" onchange="atualizarTotal()"></td>
-                <td><label class='item' for="produto${index}">${produto.nome} (R$${produto.preco.toFixed(2)})</label></td>
-                <td style="width:15%; text-align: right"><label class='quant' >Qtd:<input type="number" style="width: 35%" id="quantidade${index}" value=" " min="0" onchange="atualizarTotal()"></label</td>
-                <td style="width:15%"><button class="botao" onclick="adicionarItens()"><img src="adiciona.png" alt="Adicionar" width="20" height="20"></button></td>
-            </tr>
-        </table>
-    </div>`;
-    listaProdutos.innerHTML += produtoHTML;
-                    
+                    <div class="produto" style="width:100%">
+                        <table style="width:100%">
+                            <tr style="width:100%">
+                                <td style="width:auto"><input type="checkbox" id="produto${index}" data-tipo="${produto.tipo}" data-produto="${produto.nome}" data-preco="${produto.preco}" onchange="atualizarTotal()"></td>
+                                <td><label class='item' for="produto${index}">${produto.nome} (R$${produto.preco.toFixed(2)})</label></td>
+                                <td style="width:15%; text-align: right"><label class='quant' >Qtd:<input type="number" style="width: 35%" id="quantidade${index}" value=" " min="0" onchange="atualizarTotal()"></label</td>
+                                <td style="width:15%"><button class="botao" onclick="adicionarItens()"><img src="adiciona.png" alt="Adicionar" width="20" height="20"></button></td>
+                            </tr>
+                        </table>
+                    </div>`;
+                    listaProdutos.innerHTML += produtoHTML;
                 });
             }
             atualizarTotal();
