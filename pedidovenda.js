862f2ba6f53801b43a7c562df0943f18f216cd5f
Patrícia Lopes <102095019+ProfPatLopes@users.noreply.github.com>
2025-04-07 10:06:08 -0300

    ---

--- pedidovenda.js
+++ pedidovenda.js
@@ -298,7 +298,7 @@ function imprimirPedido2() {
                 detalhesPedido += `
                 <table style="width: auto; font-size: 15px;">
                 <tr>
-                <td colspan='4' style="background-color: black; color: white;text-align: center; font-size: 16px;width:100%"><strong>>--------VIA BAR--------<</strong></td>
+                <td colspan='4' style="background-color: black; color: white;text-align: center; font-size: 16px;width:100%"><strong>>----------VIA BAR----------<</strong></td>
             </tr>
             <tr >
                 <td colspan="4" style="width: auto; text-align: left; width: 100%;">Data/hora: ${data}</td>
@@ -336,7 +336,7 @@ function imprimirPedido2() {
         <table style="width: auto; font-size: 15px;">
         
             <tr>
-                <td colspan='4' style="background-color: black; color: white;text-align: center; font-size: 16px;width:100%"><strong>>----VIA COZINHA----<</strong></td>
+                <td colspan='4' style="background-color: black; color: white;text-align: center; font-size: 16px;width:100%"><strong>>------VIA COZINHA------<</strong></td>
             </tr>
             <tr >
                 <td colspan="4" style="width: auto; text-align: left; width: 100%;">Data/hora: ${data}</td>
